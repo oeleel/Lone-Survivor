0c61dc12b5aa06fd957e098dbc12f3ca431b9e1a
Leo Lee <leosungwonlee@gmail.com>
2023-11-23 16:09:08 -0500

    added player movement; justin code
    moves player towards point

--- final_project.py
+++ final_project.py
@@ -3,7 +3,15 @@ import uvage
 camera = uvage.Camera(800, 600)
 
 sheet = uvage.load_sprite_sheet('sprite_sheet_cs1110.png', 4, 4)
-player = uvage.from_image(200,200, sheet[8])
+player = uvage.from_image(200,200, sheet[0])
+
+def player_move(): # might need to use slope to get character to move in the direction we please
+    if camera.mouseclick:
+
+
+
+
+
 
 def tick():
     camera.draw(player)
