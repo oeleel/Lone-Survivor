cc18546e34da3ac6ca1f7524786c692cef6ecc07
Leo Lee <leosungwonlee@gmail.com>
2023-12-03 15:39:40 -0500

    q_ability and score display
    has working q ability and score on screen

--- final_project.py
+++ final_project.py
@@ -4,14 +4,30 @@ import random
 
 camera = uvage.Camera(800, 600)
 
+score = 0
+
 player_images = uvage.load_sprite_sheet('sprite_sheet_cs1110.png', 4, 4)
 player = uvage.from_image(200,200, player_images[0])
 point = uvage.from_circle(400, 300, 'red', 5) #used to show mouseclick on screen
-q_ability = uvage.from_image(240, player.y, 'qAbility.png')
+score_display = uvage.from_text(80,30, 'Score: ' + str(score), 50, 'red')
+
+q_ability = uvage.from_image(-1000, -1000, 'qAbility.png')
 q_ability.scale_by(0.05)
+ability_off_loc = uvage.from_circle(-1000, -1000, 'red', 1)
+q_og_x = -1000
+q_og_y = -1000
+q_ability_out = False
+q_key_was_pressed = False
 
 ult_orb = uvage.from_circle(400, 400, 'blue', 20)
 ult_range = uvage.from_circle(-400, -500, 'blue', 200)
+
+has_ult = False
+frames = 0
+
+current_x = 0
+current_y = 0
+position_captured = False
 
 y_down_increment = 0 #goes to 3
 x_left_increment = 4 #goes to 7
@@ -21,8 +37,9 @@ y_up_increment = 12 #goes to 15
 last_x_direction = 'right'
 last_y_direction = 'down'
 
-#flipped = False
 enemies = []
+
+
 
 def player_move():
     global y_up_increment, y_down_increment, x_right_increment, x_left_increment, last_x_direction, last_y_direction
@@ -49,8 +66,10 @@ def player_move():
 
     player.x += tx
     player.y += ty
-    q_ability.x += tx
-    q_ability.y += ty
+
+    if not q_ability_out:
+        q_ability.x += tx
+        q_ability.y += ty
 
     is_moving = hypo > 1  # Check if the player is far enough
     idle_bound = 4
@@ -139,10 +158,7 @@ def chase(): #makes enemy follow player
             enem.y += -ty
 
 
-        for q in enemies: #enemies cant overlap
-            for w in enemies:
-                if q != w:
-                    q.move_to_stop_overlapping(w)
+
 
 
         '''if player.x > enem.x and not flipped:      NOT WORKING NEED TO FIX, trying to make it so that enemy always faces player
@@ -156,37 +172,32 @@ def chase(): #makes enemy follow player
 '''
 
 
-def ult():
-    #if player.touches(ult_orb):
-
-#def fireball():
-    q_ability = uvage.from_image(player.left, player.y, 'qAbility.png')
-    '''if uvage.is_pressing('q'):
-        y_abs = abs(q_ability.y - mousey)
-        x_abs = abs(q_ability.x - mousex)
-        y_dist = q_ability.y - mousey
-        x_dist = q_ability.x - mousex
-
-        hypo1 = math.sqrt(y_dist ** 2 + x_dist ** 2)
-        speed = 7
-
-        if hypo1 != 0:
-            tx = (x_dist / hypo1) * speed
-            ty = (y_dist / hypo1) * speed
+def spawn_ult():
+    global frames, has_ult
+    if has_ult == False:
+        if frames == 240:
+            ult_orb.x = random.randint(100, 700)
+            ult_orb.y = random.randint(100, 500)
+            frames = 0
         else:
-            tx = 0
-            ty = 0
+            frames += 1
 
-        player.x += tx
-        player.y += ty
-        q_ability.x += tx
-        q_ability.y += ty
+def grab_ult():
+    global has_ult
+    if player.touches(ult_orb):
+        has_ult = True
+        ult_orb.x = -100
+        ult_orb.y = -100
 
 
 
+def use_ult():
+    global has_ult
+    if uvage.is_pressing("r") and has_ult == True:
+        has_ult = False
         touch = [] #must create list bc pop makes list shorter
-        ult_range.x = ult_orb.x
-        ult_range.y = ult_orb.y
+        ult_range.x = player.x
+        ult_range.y = player.y
         for q in range(len(enemies), ):
             if ult_range.touches(enemies[q]):
                 touch.append(q)
@@ -194,26 +205,88 @@ def ult():
             enemies.pop(w)
         ult_range.x = -400
         ult_range.y = -500
-        '''
 
+def fireball():
+    global q_ability_out, q_ability_dx, q_ability_dy, current_x, current_y, position_captured, q_og_x, q_og_y
+
+    # Check if positions are stored and the ability is not already active
+    if uvage.is_pressing('q') and not q_ability_out:
+        if not position_captured:
+            q_ability.x = player.x
+            q_ability.y = player.y
+
+            # Capture and store the mouse's position at the moment 'q' is pressed
+            stored_mouse_x = camera.mousex
+            stored_mouse_y = camera.mousey
+            position_captured = True
+
+            # Calculate the direction from q_ability's current position to the stored mouse position
+            q_ability_dx = stored_mouse_x - player.x
+            q_ability_dy = stored_mouse_y - player.y
+
+            # Normalize the direction
+            distance = math.sqrt(q_ability_dx ** 2 + q_ability_dy ** 2)
+            if distance != 0:
+                q_ability_dx /= distance
+                q_ability_dy /= distance
+
+            # Activate the ability
+            q_ability_out = True
+
+    # If the ability is active, update its position
+    if q_ability_out:
+        # Move q_ability in the calculated direction at a fixed speed (e.g., 7)
+        q_ability.x += q_ability_dx * 10
+        q_ability.y += q_ability_dy * 10
+
+        # Check if q_ability is off the screen
+        if q_ability.x > camera.width or q_ability.x < 0 or q_ability.y > camera.height or q_ability.y < 0:
+            # Reset q_ability's position to the current player's position and deactivate it
+            q_ability.x = ability_off_loc.x
+            q_ability.y = ability_off_loc.y
+            q_ability_out = False
+            position_captured = False
+
+def enemy_kill():
+    global score, score_display
+    enemy_touched = False
+    for enemy in enemies:
+        if q_ability.touches(enemy) and not enemy_touched:
+            enemy.x = 1000
+            enemy.y = 1000
+            q_ability.x = ability_off_loc.x
+            q_ability.y = ability_off_loc.y
+            score += 1
+            score_display = uvage.from_text(80, 30, 'Score: ' + str(score), 50, 'red')
+            enemy_touched = True
 
 
 def tick():
+    global score_display, score
+
     camera.clear('black')
+
+    score_display.text = 'Score: ' + str(score)
+
     camera.draw(player)
     camera.draw(point)
 
-
-    #camera.draw(ult_orb)
-    #camera.draw(ult_range)
-    ult()
+    camera.draw(ult_orb)
+    camera.draw(ult_range)
+    spawn_ult()
+    grab_ult()
+    use_ult()
 
     camera.draw(q_ability)
 
     player_move()
     spawn_enemy()
-    #fireball()
+    enemy_kill()
+    fireball()
     chase()
+
+    camera.draw(score_display)
+
     for i in enemies:
         camera.draw(i)
     camera.display()
